f40aed42169d96b5b54784d68a86f5c548c7baaa
Stahnev Yury <stahnev@stael.ru>
2022-11-29 14:32:05 +0300

    init

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const chalk = require('chalk');
+const { urlencoded } = require('express');
+const initDatabase = require('./startUp/initDatabase');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+
+app.use('/api', routes);
+
+const PORT = config.get('port') ?? 8080;
+
+// if (process.env.NODE_ENV === "production") {
+//     console.log("Production");
+// }
+// if (process.env.NODE_ENV === "development") {
+//     console.log("development");
+// }
+
+async function start() {
+   try {
+      mongoose.connection.once('open', () => {
+         initDatabase();
+      });
+      await mongoose.connect(config.get('mongoUri'));
+      console.log(chalk.blue('MongoDB connected'));
+      app.listen(PORT, () => {
+         console.log(chalk.green(`Server has been started on port ${PORT}...`));
+      });
+   } catch (e) {
+      console.log(chalk.red(e.message));
+      process.exit(1);
+   }
+}
+
+start();
